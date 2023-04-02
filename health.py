30b5c429c35cecae0ca2066bf3410d70f5fc1245
CaioCapitani <caiocapitani22@gmail.com>
2023-04-02 18:41:12 -0300

    Refatoração do código de cura da personagem: introdução da funcionalidade de ajuste da quantidade de cura com base no nível de dificuldade. Inclusão da variável "difficulty" e cálculo da quantidade de cura pela divisão do valor gerado aleatoriamente pelo nível de dificuldade. Maior flexibilidade e personalização para o jogo.

--- health.py
+++ health.py
@@ -1,5 +1,11 @@
-import random
+import random # importando a biblioteca random
 
-health = 50
+health = 50 # atribuindo o valor de vida inicial da personagem
 
-potion_health = random.randint (25,50)
+difficulty = 1 # o número representa a dificuldade do jogo
+
+potion_health = int(random.randint (25,50) / difficulty) #quanto maior a dificuldade menor a quantidade de vida recupera
+
+health = health + potion_health
+
+print (health) # mostra o novo valor da vida
